804f0d15b252be4b16b78fef1bddaac574b12f5e
JEON SEUNGBHIN <tmdqls104@naver.com>
2026-03-23 20:36:51 +0900

    refactor: move apiFetch and ApiError to lib/api

--- src/utils/serverRequest.ts
+++ src/utils/serverRequest.ts
@@ -1,5 +1,5 @@
 import { HttpStatusCode } from '@/constants/httpStatusCode';
-import { ApiError } from '@/app/api/apiError';
+import { ApiError } from 'next/dist/server/api-utils';
 
 export const serverRequest = async (url: string, token: string, options: RequestInit = {}) => {
   const baseURL = process.env.BASE_URL;
