46dac0e21daafb3eaa483f93678a46cc3e6230b4
JEON SEUNGBHIN <tmdqls104@naver.com>
2026-04-23 21:37:48 +0900

    refactor: move app shell to root layout

--- src/app/(provider)/(root)/home/page.tsx
+++ src/app/(provider)/(root)/home/page.tsx
@@ -1,10 +1,5 @@
-import AppShell from '@/components/layout/AppShell';
 import HomePageContent from './_components/HomePageContent';
 
 export default async function HomePage() {
-  return (
-    <AppShell>
-      <HomePageContent />
-    </AppShell>
-  );
+  return <HomePageContent />;
 }
