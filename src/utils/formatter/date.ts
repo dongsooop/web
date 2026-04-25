35cfe4224cc91e98eb97da42d6bc95b8b4d688b2
JEON SEUNGBHIN <tmdqls104@naver.com>
2026-04-25 23:46:19 +0900

    feat:update date formatter utilities

--- src/utils/formatter/date.ts
+++ src/utils/formatter/date.ts
@@ -1,17 +1,31 @@
-export const getTodayLabel = () => {
-  const today = new Date();
-  const month = today.getMonth() + 1;
-  const date = today.getDate();
-  const dayOfWeek = ['일', '월', '화', '수', '목', '금', '토'][today.getDay()];
-  
-  return `${month}월 ${date}일 (${dayOfWeek})`;
-};
+const weekLabels = ['일', '월', '화', '수', '목', '금', '토'] as const;
 
-export function formatDateLabel(dateKey: string) {
-  const [year, month, day] = dateKey.split('-').map(Number);
-  return `${year}년 ${month}월 ${day}일`;
+function toDate(value: Date | string) {
+  if (value instanceof Date) {
+    return value;
+  }
+
+  return new Date(value);
 }
 
-export function formatMonthLabel(date: Date) {
+export function formatMonthLabel(value: Date | string) {
+  const date = toDate(value);
   return `${date.getMonth() + 1}월`;
+}
+
+export function formatDateLabel(value: Date | string) {
+  const date = toDate(value);
+  return `${date.getMonth() + 1}월 ${date.getDate()}일`;
+}
+
+export function getTodayLabel() {
+  const today = new Date();
+  const week = weekLabels[today.getDay()];
+
+  return `${today.getMonth() + 1}월 ${today.getDate()}일 ${week}요일`;
+}
+
+export function formatYmdDot(date: string) {
+  const [year = '', month = '', day = ''] = date.split('T')[0]?.split(' ')[0]?.split('-') ?? [];
+  return `${year}.${month}.${day}`;
 }
